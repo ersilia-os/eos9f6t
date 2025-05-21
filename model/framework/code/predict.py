40f2d3507e6234287baad5c821e7eb825049c41d
GemmaTuron <gemma@ersilia.io>
2025-05-21 13:06:49 +0200

    update model

--- model/framework/code/predict.py
+++ model/framework/code/predict.py
@@ -3,13 +3,16 @@ import os, sys
 import pandas as pd
 from chemprop.train import chemprop_predict
 
+
+
 #intermediate features file created
 features_file = sys.argv[-1]
-output_file = sys.argv[3]
+output_file = sys.argv[7]
 
 if __name__ == '__main__':
     chemprop_predict()
     df = pd.read_csv(output_file)
-    print(df.head())
+    df = df[["activity"]]
+    df.to_csv(output_file, index=False)
     #Remove intermediate features file after making predictions
     os.remove(features_file)
